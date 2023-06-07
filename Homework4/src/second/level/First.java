57bf849fa450ab2d2b89be2002c0ee19b027c2f7
Sasza44 <110614574+Sasza44@users.noreply.github.com>
2023-06-07 21:13:01 +0300

    Add files via upload

--- Homework4/src/second/level/First.java
+++ Homework4/src/second/level/First.java
@@ -9,19 +9,28 @@ public class First {
 		int height;
 		System.out.println("Write width ");
 		height = sc.nextInt();
-		
-		for(int i = 1; i <= height * 2 - 1; i++) {
-			if(i <= height) {
-				for(int j = 1; j <= i; j++) {
-					System.out.print("*");
+			
+		int starNum = 0; // кількість зірочок
+		int starNumInRow = 1; // кількість зірочок в ряду
+		int rowNum = 0; // кількість рядів
+		while(rowNum != 2 * height - 1) {
+			starNum += 1;
+			System.out.print("*");
+			if(rowNum < height - 1) {
+				if(starNum == starNumInRow) {
+					System.out.println();
+					rowNum += 1;
+					starNumInRow += 1;
+					starNum = 0;
 				}
-				System.out.println();
 			}
 			else {
-				for(int j = 1; j <= 2 * height - i; j++) {
-					System.out.print("*");
+				if(starNum == starNumInRow) {
+					System.out.println();
+					rowNum += 1;
+					starNumInRow -= 1;
+					starNum = 0;
 				}
-				System.out.println();
 			}
 		}
 	}
